b3a559b8c0a215dd21efa62cd66304c471ce9c7f
John Kasiewicz <crestcode@gmail.com>
2014-05-05 10:12:27 -0400

    end of section 3

--- section 3/app/controllers/customersController.js
+++ section 3/app/controllers/customersController.js
@@ -1,10 +1,18 @@
-function CustomersController($scope) {
-    $scope.sortBy = 'name';
-    $scope.reverse = false;
+(function() {
+    var CustomersController = function ($scope) {
+        $scope.sortBy = 'name';
+        $scope.reverse = false;
 
-    $scope.customers = [{name:'John', city:'New York', joined:'2014-01-02'},{name:'Tom', city:'Austin', orderTotal:12},{name:'Jackie', city:'Los Angeles', orderTotal:14.39},{name:'John', city:'Seattle', orderTotal:44.444432}]
-    $scope.doSort = function(propName) {
-        $scope.sortBy = propName;
-        $scope.reverse = !$scope.reverse;
+        $scope.customers = [{name:'John', city:'New York', joined:'2014-01-02'},{name:'Tom', city:'Austin', orderTotal:12},{name:'Jackie', city:'Los Angeles', orderTotal:14.39},{name:'John', city:'Seattle', orderTotal:44.444432}]
+        $scope.doSort = function(propName) {
+            $scope.sortBy = propName;
+            $scope.reverse = !$scope.reverse;
+        };
     };
-}
+
+    // dealing with minification
+    CustomersController.$inject = ['$scope'];
+
+    angular.module('customersApp')
+        .controller('CustomersController', CustomersController);
+}());
